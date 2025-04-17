900003d5c4de7e07405c79967aa1ef1e1f2f68df
robeanobug <fakeemail@gmail.com>
2025-04-17 14:40:28 -0400

    dynamic grid applied, boxes turn solid pink when mouse hovers over it

--- etch.js
+++ etch.js
@@ -1,12 +1,24 @@
 const container = document.querySelector(".container");
+let gridSize = 100;
 
 function makeGrid(size) {
-    const totalSquares = size ** 2
+    const totalSquares = size ** 2;
+    const squareSize = 640 / size;
+
     for (let i = 0; i < totalSquares; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
         container.appendChild(square);
     }
+
+    const squares = document.querySelectorAll(".square")
+    squares.forEach(square => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "pink"; 
+        });
+    });
 }
 
-makeGrid(16);
+makeGrid(gridSize);
